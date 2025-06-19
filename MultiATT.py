8bb6278bc597b75dc499ad4e4013a41ce2371cc6
minmin <81370249+asdfghjkl-ai@users.noreply.github.com>
2025-06-19 18:35:21 +0800

    Update MultiATT.py

--- MultiATT.py
+++ MultiATT.py
@@ -3,15 +3,9 @@ import torch.nn as nn
 
 class ConvBNReLU(nn.Module):
     def __init__(self, in_channel, out_channel, k=3, s=1, p=1, g=1, d=1, bias=False, bn=True, relu=True):
-        # Input channel in_channel, output channel out_channel, kernel size k,
-        # stride s, padding p, group convolution g, dilation rate d,
-        # whether to include bias term bias, whether to apply batch normalization bn,
-        # whether to apply ReLU activation relu
 
         super(ConvBNReLU, self).__init__()
 
-        # During initialization, create a sequence 'conv' containing
-        # a convolutional layer, batch normalization layer, and ReLU activation function
         conv = [nn.Conv2d(in_channel, out_channel, k, s, p, dilation=d, groups=g, bias=bias)]
 
         # Add batch normalization layer if specified
@@ -22,7 +16,6 @@ class ConvBNReLU(nn.Module):
         if relu:
             conv.append(nn.ReLU(inplace=True))
 
-        # Create a sequential container with the defined layers
         self.conv = nn.Sequential(*conv)
 
     def forward(self, x):
@@ -38,12 +31,7 @@ class DSConv3x3(nn.Module):
         super(DSConv3x3, self).__init__()
 
         self.conv = nn.Sequential(
-            # The first convolutional layer is a depthwise separable convolution
             ConvBNReLU(in_channel, in_channel, k=3, s=stride, p=dilation, d=dilation, g=in_channel),
-
-            # The second convolutional layer is a 1x1 pointwise convolution
-            # This helps in combining features from the depthwise convolution
-            # 'ReLU' activation is applied if specified
             ConvBNReLU(in_channel, out_channel, k=1, s=1, p=0, relu=relu)
         )
 
@@ -64,8 +52,6 @@ class TAMM(nn.Module):
             DSConv3x3(channel, channel, stride=1, dilation=d) for d in dilation_level
         ])  # List of branches with dilated convolutions
 
-        # Spatial Pyramid Attention Network (SPAN)
-        # Adaptive average pooling at different spatial levels
         self.avg_pool1 = nn.AdaptiveAvgPool2d(1)
         self.avg_pool2 = nn.AdaptiveAvgPool2d(2)
         self.avg_pool4 = nn.AdaptiveAvgPool2d(4)
@@ -82,8 +68,6 @@ class TAMM(nn.Module):
         # Initial convolution operation
         conv = self.conv(x)
 
-        # Apply dilated convolutions on different branches
-        # Concatenate the results and perform element-wise summation
         brs = [branch(conv) for branch in self.branches]
         brs.append(conv)
         gather = sum(brs)
